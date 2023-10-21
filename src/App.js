bf8e169cfc84cf3bab6fde2f0ac8243ca1adbea5
Nikhita-Gorja <138364937+Nikhita-Gorja@users.noreply.github.com>
2023-10-21 20:54:46 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react';
+import AgeCalculator from './components/AgeCalculator';
+
+function App() {
+
+  return (
+    <>
+
+      {/* REACT TASK-5 */}
+      <div className="App">
+        <AgeCalculator />
+      </div>
+    </>
+
+  );
+}
+
+export default App;
